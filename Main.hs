d636d6b8b2271e2680356471f9aabf3e51cf917f
Chronoes <martentarkin@gmail.com>
2016-09-20 22:18:44 +0300

    Parse and make HTTP requests and responses, restructured files for server testing

--- Main.hs
+++ Main.hs
@@ -4,11 +4,11 @@ import Data.Maybe
 
 import Parser
 
-main = serve (Host "localhost") "1337" $ \(connectionSocket, remoteAddr) -> do
+main = serve HostAny "1337" $ \(connectionSocket, remoteAddr) -> do
     putStrLn $ "TCP connection established from " ++ show remoteAddr
     req <- recv connectionSocket 1024
 
     let request = BS.lines . fromMaybe BS.empty $ req
     print . getRequestAndHeaders $ request
 
-    send connectionSocket (BS.pack "HTTP/1.1 200 OK\r\n\r\n")
+    send connectionSocket $ makeResponse (Response (BS.pack "HTTP/1.1") 200) []
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,10 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Parser (
-getRequestAndHeaders
+MainHeader(..),
+Header,
+getRequestAndHeaders,
+getResponseAndHeaders,
+makeRequest,
+makeResponse
 )
 where
 
+
 import Data.Char
+import Data.Maybe
 import qualified Data.ByteString.Char8 as BS
+
+trim :: BS.ByteString -> BS.ByteString
+trim "" = ""
+trim b = (fst . BS.spanEnd isSpace) . BS.dropWhile isSpace $ b
+
+eol :: BS.ByteString -> BS.ByteString
+eol = (`BS.append` "\r\n")
+
+endOfHeader :: BS.ByteString -> Bool
+endOfHeader t = BS.null t || t == "\r"
 
 
 data Header = Header {
@@ -13,24 +32,54 @@ data Header = Header {
 } deriving (Eq, Ord, Show)
 
 parseToHeader :: BS.ByteString -> Header
-parseToHeader bytes = Header name (BS.init . BS.tail $ value)
-    where (name, value) = BS.break (== ':') bytes
+parseToHeader b = Header name (trim . BS.tail $ value)
+    where (name, value) = BS.break (== ':') b
+
+headerToString :: Header -> BS.ByteString
+headerToString h = name h `BS.append` ": " `BS.append` value h
+
+headersToString :: [Header] -> BS.ByteString
+headersToString = BS.concat . map (eol . headerToString)
 
 
-data Request = Request {
+data MainHeader = Request {
     method :: BS.ByteString,
     uri :: BS.ByteString,
-    protocol :: BS.ByteString
+    reqProtocol :: BS.ByteString
+} | Response {
+    resProtocol :: BS.ByteString,
+    statusCode :: Int
 } deriving (Eq, Show)
 
-parseRequest :: BS.ByteString -> Request
-parseRequest bytes = Request (head list) (list !! 1) (list !! 2)
-    where list = BS.words bytes
 
 
-notEndOfHeader :: BS.ByteString -> Bool
-notEndOfHeader = (`notElem` [BS.singleton '\r', BS.empty])
+parseRequest :: BS.ByteString -> MainHeader
+parseRequest b = Request (head list) (list !! 1) (list !! 2)
+    where list = BS.words b
+
+parseResponse :: BS.ByteString -> MainHeader
+parseResponse b = Response protocol (fst . fromMaybe (0, "") . BS.readInt $ status)
+    where (protocol, status) = BS.break (== ' ') b
 
 
-getRequestAndHeaders :: [BS.ByteString] -> (Request, [Header])
-getRequestAndHeaders req = (parseRequest . head $ req, map parseToHeader . takeWhile notEndOfHeader . tail $ req)
+getMainHeaderAndHeaders :: (BS.ByteString -> MainHeader) -> [BS.ByteString] -> (MainHeader, [Header])
+getMainHeaderAndHeaders f req = (f . head $ req, map parseToHeader . takeWhile (not . endOfHeader) . tail $ req)
+
+getRequestAndHeaders = getMainHeaderAndHeaders parseRequest
+
+getResponseAndHeaders = getMainHeaderAndHeaders parseResponse
+
+
+makeRequest :: MainHeader -> [Header] -> BS.ByteString
+makeRequest req = eol
+    . BS.append (eol . BS.unwords $ [method req, uri req, reqProtocol req])
+    . headersToString
+
+getStatusText :: Int -> BS.ByteString
+getStatusText 200 = "200 OK"
+getStatusText _ = error "Status code does not exist"
+
+makeResponse :: MainHeader -> [Header] -> BS.ByteString
+makeResponse r = eol
+    . BS.append (eol . BS.unwords $ [resProtocol r, getStatusText . statusCode $ r])
+    . headersToString
